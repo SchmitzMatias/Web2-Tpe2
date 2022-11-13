3871dbe50411304ace33e16a52bc2c7f0200da17
Matias <matias.schmitz@gmail.com>
2022-11-13 17:44:06 -0300

    ammend

--- api-router.php
+++ api-router.php
@@ -1,7 +1,6 @@
 <?php
 
 require_once './libs/Router.php';
-require_once './app/controllers/product-api.controller.php';
 require_once './app/controllers/category-api.controller.php';
 
 $router = new Router();
@@ -11,12 +10,5 @@ $router->addRoute('categories/:id', 'GET', 'CategoryApiController', 'getCategory
 $router->addRoute('categories', 'POST', 'CategoryApiController', 'addCategory');
 $router->addRoute('categories/:id', 'PUT', 'CategoryApiController', 'updateCategory');
 $router->addRoute('categories/:id', 'DELETE', 'CategoryApiController', 'deleteCategory');
-
-
-$router->addRoute('products', 'GET', 'ProductApiController', 'getProducts');
-$router->addRoute('products/:id', 'GET', 'ProductApiController', 'getProduct');
-$router->addRoute('products', 'POST', 'ProductApiController', 'addProduct');
-$router->addRoute('products/:id', 'PUT', 'ProductApiController', 'updateProduct');
-$router->addRoute('products/:id', 'DELETE', 'ProductApiController', 'deleteProduct');
 
 $router->route($_GET["resource"], $_SERVER['REQUEST_METHOD']);
